0be3071feaf936a0b9c6a3de8db465a653c59573
Kieran Lucid <kieran@Kierans-MacBook-Pro.local>
2026-02-16 18:01:03 +0000

    test

--- main.go
+++ main.go
@@ -123,7 +123,7 @@ func runWizard(configPath string) QiConfig {
 	reader := bufio.NewReader(os.Stdin)
 	fmt.Println("🧙 Qi Setup Wizard")
 
-	fmt.Print("📂 Enter QHOME path: ")
+	fmt.Print("📂 Enter QHOME path (where kc.lic is stored): ")
 	qhome, _ := reader.ReadString('\n')
 	qhome = strings.TrimSpace(qhome)
 
